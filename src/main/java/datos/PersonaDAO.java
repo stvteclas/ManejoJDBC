939cf2b60deb5d15ca8fcd8497bad5f63a5c9c91
stvteclas <stvpablo@gmail.com>
2022-08-08 14:54:32 -0300

    prueba

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -14,7 +14,9 @@ import static datos.Conexion.*;
 
 public class PersonaDAO {
     private static final String SQL_SELECT= "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
-
+    private static final String SQL_INSERT=" INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?, ?, ?, ?)";
+    private static final String SQL_UPDATE="UPDATE persona SET nombre=?, apellido=?, email=?, telefono=? WHERE id_persona=?";
+    private static final String SQL_DELETE= "DELETE FROM persona WHERE id_persona=?";
     public List<Persona> seleccionar(){
         Connection conn=null;
         PreparedStatement stmt= null;
@@ -42,9 +44,9 @@ public class PersonaDAO {
         }
         finally {
             try {
-                Conexion.close(rs);
-                Conexion.close(stmt);
-                Conexion.close(conn);
+                close(rs);
+                close(stmt);
+                close(conn);
 
             } catch (SQLException e) {
                 e.printStackTrace(System.out);
@@ -54,5 +56,93 @@ public class PersonaDAO {
         return personas;
 
 
+    }
+
+    public int insertar(Persona persona){
+        Connection conn=null;
+        PreparedStatement stmt=null;
+        int registros=0;
+        try {
+            conn=getConnection();
+            stmt=conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            registros= stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        finally {
+            try {
+                close(stmt);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+            try {
+                close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+
+    public int actualizar(Persona persona){
+        Connection conn=null;
+        PreparedStatement stmt=null;
+        int registros=0;
+        try {
+            conn=getConnection();
+            stmt=conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            stmt.setInt(5, persona.getIdPersona());
+            registros= stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        finally {
+            try {
+                close(stmt);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+            try {
+                close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+
+    public int eliminar(Persona persona){
+        Connection conn=null;
+        PreparedStatement stmt=null;
+        int registros=0;
+        try {
+            conn=getConnection();
+            stmt=conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, persona.getIdPersona());
+            registros= stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        finally {
+            try {
+                close(stmt);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+            try {
+                close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        return registros;
     }
 }
--- src/main/java/test/TestManejoPersonas.java
+++ src/main/java/test/TestManejoPersonas.java
@@ -9,8 +9,36 @@ public class TestManejoPersonas {
     public static void main(String[] args) {
         PersonaDAO personadao= new PersonaDAO();
         List<Persona> personas=personadao.seleccionar();
-        for(Persona persona:personas){
+        personas.forEach(persona -> {
             System.out.println("persona = " + persona);
-        }
+        });
+
+        Persona personaNueva= new Persona("Carlos", "Esparza","cesparza@mail.com", "554456587");
+        personadao.insertar(personaNueva);
+        personas = personadao.seleccionar();
+        personas.forEach(persona -> {
+            System.out.println("persona = " + persona);
+        });
+
+        Persona personaNew= new Persona(5,"Romina", "Balquinta","ro_balquuinta@gmail.com", "554456587");
+        personadao.actualizar(personaNew);
+        personas = personadao.seleccionar();
+        personas.forEach(persona -> {
+            System.out.println("persona = " + persona);
+        });
+
+
+        Persona personaEliminar= new Persona(5);
+        personadao.eliminar(personaEliminar);
+        personas = personadao.seleccionar();
+        personas.forEach(persona -> {
+            System.out.println("persona = " + persona);
+        });
+
+
+
+
     }
+
+    
 }
